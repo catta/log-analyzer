bb1839a070f0543bc67e50af5b9105ea3c2028cc
unknown <catalinc@LTRO-0011.ceragon.com>
2016-03-12 17:50:46 +0200

    more elasticsearch tutorial code

--- log-analyzer/src/main/java/com/ceragon/elasticsearch/ElasticsearchClient.java
+++ log-analyzer/src/main/java/com/ceragon/elasticsearch/ElasticsearchClient.java
@@ -1,11 +1,14 @@
 package com.ceragon.elasticsearch;
 
+import java.io.IOException;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 
+import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.get.MultiGetItemResponse;
 import org.elasticsearch.action.get.MultiGetResponse;
+import org.elasticsearch.action.index.IndexRequestBuilder;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.AdminClient;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.IndicesAdminClient;
@@ -13,54 +16,102 @@ import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.client.transport.TransportClient.Builder;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.node.Node;
 import org.elasticsearch.node.NodeBuilder;
 
 public class ElasticsearchClient
 {
     
-    public static void main(String[] args)
+    private static final String TYPE  = "external";
+    private static final String INDEX = "customer";
+
+    public static void main(String[] args) throws Exception
     {
         ElasticsearchClient ceragonClient = new ElasticsearchClient();
         
         // ceragonClient.nodeClient();
         
-        ceragonClient.transportClient();
+        Client client = ceragonClient.transportClient();
+        
+        String id = ceragonClient.addNewDocument(client);
+        
+        ceragonClient.getDocument(client, id);
+        
+        int millis = 5000;
+        System.out.println("sleep " + millis);
+        Thread.sleep(millis);
+
+        ceragonClient.deleteDocument(client, id);
+
+        client.close();
+        
     }
     
-    private void transportClient()
+    private void deleteDocument(Client client, String id)
     {
-        try
+        DeleteResponse response = client.prepareDelete(INDEX, TYPE, id).get();
+        if (response != null)
         {
-            Builder builder = TransportClient.builder();
-            
-            builder.settings(Settings.settingsBuilder().put("cluster.name", "elasticsearch"));
-            InetSocketTransportAddress transportAddress = new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300);
-            System.out.println("transportAddress " + transportAddress);
+            System.out.println(">> delete response " + response.getShardInfo());
+        }
+    }
 
-            TransportClient client = builder.build().addTransportAddress(transportAddress);
+    private void getDocument(Client client, String id)
+    {
+        GetResponse response = client.prepareGet(INDEX, TYPE, id).get();
+        
+        if (response != null && response.isExists())
+        {
+            System.out.println(">> get document " + response);
+        }
+    }
 
-            System.out.println("transport client " + client);
-            
-            MultiGetResponse multiGetResponse = client.prepareMultiGet().add("customer", "external", "1").get();
-            
-            for (MultiGetItemResponse itemResponse : multiGetResponse)
+    private String addNewDocument(Client client) throws IOException
+    {
+        IndexRequestBuilder index = client.prepareIndex(INDEX, TYPE);
+        XContentBuilder jsonBuilder = XContentFactory.jsonBuilder();
+        jsonBuilder.startObject().field("name", "Catalin");
+        index.setSource(jsonBuilder.endObject());
+        
+        IndexResponse indexResponse = index.get();
+        // indexResponse.getId();
+        // indexResponse.getIndex();
+        // indexResponse.getShardInfo();
+        // indexResponse.getType();
+        // indexResponse.getVersion();
+        System.out.println("add document indexResponse " + indexResponse);
+        
+        return indexResponse.getId();
+    }
+
+    private Client transportClient() throws IOException
+    {
+        Builder builder = TransportClient.builder();
+        
+        builder.settings(Settings.settingsBuilder().put("cluster.name", "elasticsearch"));
+        InetSocketTransportAddress transportAddress = new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300);
+        System.out.println("transportAddress " + transportAddress);
+        
+        TransportClient client = builder.build().addTransportAddress(transportAddress);
+        
+        System.out.println("transport client " + client);
+        
+        MultiGetResponse multiGetResponse = client.prepareMultiGet().add(INDEX, TYPE, "1").get();
+        
+        for (MultiGetItemResponse itemResponse : multiGetResponse)
+        {
+            GetResponse response = itemResponse.getResponse();
+            if (response.isExists())
             {
-                GetResponse response = itemResponse.getResponse();
-                if (response.isExists())
-                {
-                    String source = response.getSourceAsString();
-                    System.out.println("source " + source);
-                }
+                String source = response.getSourceAsString();
+                System.out.println("source " + source);
             }
-            
-            client.close();
-            
         }
-        catch (UnknownHostException e)
-        {
-            e.printStackTrace();
-        }
+
+        return client;
+
     }
 
     private void nodeClient()
